67770b74b1e595ca1afb13714309568e51eab172
Morton Kuo <menghsinkuo@gmail.com>
2022-09-19 21:15:05 +0800

    feat: remove user's group chat window if the user left the group

--- public/src/sidebar.js
+++ public/src/sidebar.js
@@ -128,6 +128,17 @@ async function drawDeleteGroupButton(groups) {
 
       groupDiv.remove();
 
+      const messages = document.getElementById('messages');
+      if (!messages || messages.dataset.socketId !== groupId) return;
+
+      const pane = document.getElementById('pane');
+      const welcome = document.createElement('h1');
+
+      welcome.innerText = 'Welcome Aboard';
+
+      pane.innerHTML = '';
+      pane.appendChild(welcome);
+
       return setMsg(response.data);
     });
   });
