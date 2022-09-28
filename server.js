deda8a60ae4deb3683c7240c91b9b1da266c51b4
Morton Kuo <menghsinkuo@gmail.com>
2022-09-28 21:13:41 +0800

    refactor: custom error type 2

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ server.listen(3000, () => {
 });
 
 app.use((err, req, res, next) => {
-  if (err.name === 'MulterError' || 'Error') return res.status(400).json({ error: err.message });
+  if (err.name === 'MulterError' || err.name === 'Error') {
+    return res.status(400).json({ error: err.message });
+  }
 
   res.status(500).json({ error: 'server error' });
 
