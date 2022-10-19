826ceb2a6ef5d49437dc0cbc3935c4b90c2988a7
Morton Kuo <menghsinkuo@gmail.com>
2022-10-19 12:17:54 +0800

    fix: mysql connection error handling

--- utils/mysql.js
+++ utils/mysql.js
@@ -1,21 +1,28 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool(
-  {
-    host: process.env.USER_DB_HOST,
-    user: process.env.USER_DB_USERNAME,
-    password: process.env.USER_DB_PASSWORD,
-    database: process.env.USER_DB_NAME,
-  },
-  { debug: true }
-);
+const pool = connect();
 
 const promisePool = pool.promise();
 
 checkConnection().then(connection => {
   if (connection) console.log('MySQL connected');
 });
+
+function connect() {
+  try {
+    const pool = mysql.createPool({
+      host: process.env.USER_DB_HOST,
+      user: process.env.USER_DB_USERNAME,
+      password: process.env.USER_DB_PASSWORD,
+      database: process.env.USER_DB_NAME,
+    });
+
+    return pool;
+  } catch (error) {
+    console.log('MySQL connection error:', error);
+  }
+}
 
 async function checkConnection() {
   const sql = `SELECT 1`;
